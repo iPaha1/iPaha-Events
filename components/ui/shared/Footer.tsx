e3bfd70396439313ebd84dddf09fcd5d4fa3af31
iPaha1 <ike4football@gmail.com>
2024-01-06 10:53:28 +0000

    Initial commit

---
+++ components/ui/shared/Footer.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image"
+import Link from "next/link"
+
+const Footer = () => {
+  return (
+    <footer className="border-t">
+      <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
+        <Link href='/'>
+          <Image 
+            src="/assets/images/logo1.svg"
+            alt="logo"
+            width={90}
+            height={28}
+          />
+        </Link>
+        <p className="text-center text-xs text-black"> Terms & Conditions  |  Policies </p>
+
+        <p className="text-center text-xs text-black">&copy; 2024 iPaha Events. All Rights reserved.</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
