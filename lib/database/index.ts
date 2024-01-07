5bf8d9ca1fb17e28a5bc2d93262314cda7ee0f3b
iPaha1 <ike4football@gmail.com>
2024-01-07 23:23:46 +0000

    added db cache back

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -2,15 +2,19 @@ import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-export const connectToDatabase = async () => {
-  if (!MONGODB_URI) throw new Error('MONGODB_URI is missing');
+let cached = (global as any).mongoose || { conn: null, promise: null };
 
-  const conn = await mongoose.connect(MONGODB_URI, {
+export const connectToDatabase = async () => {
+  if (cached.conn) return cached.conn;
+
+  if(!MONGODB_URI) throw new Error('MONGODB_URI is missing');
+
+  cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
     dbName: 'iPaha Events',
     bufferCommands: false,
-  });
+  })
 
-  console.log('Connected to MongoDB');
-  
-  return conn;
+  cached.conn = await cached.promise;
+
+  return cached.conn;
 }
